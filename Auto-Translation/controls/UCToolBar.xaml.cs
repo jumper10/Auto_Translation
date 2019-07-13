1c79a3c7deef770b6423e90810c8d83729ab8c35
jumper10 <jumper10@163.com>
2019-07-13 22:12:51 +0800

    create wpf project

---
+++ Auto-Translation/controls/UCToolBar.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Auto_Translation.controls
+{
+    /// <summary>
+    /// UCToolBar.xaml 的交互逻辑
+    /// </summary>
+    public partial class UCToolBar : UserControl
+    {
+        public UCToolBar()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
